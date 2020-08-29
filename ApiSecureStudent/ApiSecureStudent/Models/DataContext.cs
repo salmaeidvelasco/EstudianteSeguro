235f26da26edea6469529ba2e06740835260b258
salmaeidvelasco <a2019115131@estudiantes.upsa.edu.bo>
2020-08-29 10:25:45 -0400

    Con controlador de Students más autorización.

---
+++ ApiSecureStudent/ApiSecureStudent/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ApiSecureStudent.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext //Está relacionado con la base de dato
+    {
+        public DataContext(): base("DefaultConnection")//ctor tab tab constructor el DefaultCOn lo sacamos de web config en addname
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<ApiSecureStudent.Models.Student> Students { get; set; }
+    }
+}
---
+++ ApiSecureStudent/ApiSecureStudent/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace ApiSecureStudent.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+        [Range(1, 9999)]
+        public int StudentID { get; set; }
+
+
+        [StringLength(30)]//Permite hasta 30 caracteres
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string LastName { get; set; }
+
+
+        [StringLength(30, ErrorMessage = "The field {0} must contain betwen {2} and {1} characters", MinimumLength = 2)]//El cero hace referencia al firt name
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+    }
+}
