723f081df554f1ee37f61210cee3276743ed1aa2
salmaeidvelasco <a2019115131@estudiantes.upsa.edu.bo>
2020-09-19 02:38:37 -0400

    Se agregaron los UnitTest y todo está correcto.

---
+++ ApiSecureStudent/EstudianteSeguro.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EstudianteSeguro.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EstudianteSeguro.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("937bcf81-9ab1-4123-a7d6-cce674890325")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ApiSecureStudent/EstudianteSeguro.Test/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Results;
+using ApiSecureStudent.Controllers;
+using ApiSecureStudent.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EstudianteSeguro.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGet()
+        {
+            //Arrange
+            StudentsController controller = new StudentsController();
+
+
+            //Act
+            var listaestudiante = controller.GetStudents();
+
+            //Assert
+            Assert.IsNotNull(listaestudiante);
+        }
+
+        [TestMethod]
+        public void TestMethodPost()
+        {
+            //Arrange
+            StudentsController controller = new StudentsController();
+            Student prueba = new Student()
+            {
+                FirstName = "Salma",
+                LastName = "Eid",
+                StudentID = 1,
+                EnrollmentDate = DateTime.Now
+            };
+
+            //Act
+            IHttpActionResult actionResult = controller.PostStudent(prueba);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<Student>;
+            
+            //Assert
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual("DefaultApi", createdResult.RouteName);
+            Assert.AreEqual(prueba.StudentID, createdResult.RouteValues["id"]);
+            Assert.AreEqual("Salma", createdResult.Content.FirstName);
+
+        }
+
+
+        //Delete
+        [TestMethod]
+        public void TestMethodDelete()
+        {
+            // Arrange
+            StudentsController controller = new StudentsController();
+            Student prueba = new Student()
+            {
+                FirstName = "Salma",
+                LastName = "Eid",
+                StudentID = 1,
+                EnrollmentDate = DateTime.Now
+
+            };
+
+            // Act
+            IHttpActionResult actionResult = controller.PostStudent(prueba);
+            IHttpActionResult actionResultDelete = controller.DeleteStudent(prueba.StudentID);
+
+
+            // Assert
+            Assert.IsInstanceOfType(actionResultDelete, typeof(OkNegotiatedContentResult<Student>));
+        }
+
+
+
+        //Put
+        [TestMethod]
+        public void TestMethodPut()
+        {
+            //Arrange
+            StudentsController controller = new StudentsController();
+            Student prueba = new Student()
+            {
+                FirstName = "Salma",
+                LastName = "Eid",
+                StudentID = 1,
+                EnrollmentDate = DateTime.Now
+            };
+            //Act
+            var ListaEstudiante = controller.PostStudent(prueba);
+            prueba.FirstName = "Marcela";
+            var resultadoCreado = controller.PutStudent(prueba.StudentID, prueba) as StatusCodeResult;
+
+            //Assert
+            Assert.IsNotNull(resultadoCreado);
+            Assert.AreEqual(HttpStatusCode.NoContent, resultadoCreado.StatusCode);
+            Assert.IsInstanceOfType(resultadoCreado, typeof(StatusCodeResult));
+
+
+        }
+    }
+}
+
